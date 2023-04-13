420565a80bbcc6e6de58a6640d59a4c379af0234
Reka <reka.kovacs1993@gmail.com>
2023-04-13 11:18:11 +0100

    fizzuzz improved

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,4 +1,4 @@
-values_to_return= { 15 : "FizzBuzz", 7 : "Ruzz", 3 : "Fizz", 5 : "Buzz"}
+values_to_return= {7 : "Ruzz", 3 : "Fizz", 5 : "Buzz"}
 
 # def fizz_buzz(number):
 #     if number % 5 == 0 and fizz_numbers_division():
@@ -13,8 +13,11 @@ values_to_return= { 15 : "FizzBuzz", 7 : "Ruzz", 3 : "Fizz", 5 : "Buzz"}
 
 
 def fizzbuzz (number):
+    word = ""
     for key in values_to_return:
         if number % key == 0:
-            return values_to_return[key]
-    return str(number)
+            word += values_to_return[key]
+    if word == "":
+        return str(number)
+    return word
         
