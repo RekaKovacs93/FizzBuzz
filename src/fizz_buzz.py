16d425f6804dbff002fa9198b7767ac067fd11ec
Reka <reka.kovacs1993@gmail.com>
2023-04-13 10:57:07 +0100

    fizzbuz

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,18 +1,20 @@
-#values_to_return= {3 : "Fizz", 5 : "Buzz", 15 : "FizzBuzz"}
-div_return_fizz = [3]
-def fizz_buzz(number):
-    if number % 5 == 0 and fizz_numbers_division():
-        return "FizzBuzz"
-    elif number % 3 == 0:
-        return "Fizz"
-    elif number % 5 == 0:
-        return "Buzz"
-    else:
-        return str(number)
+values_to_return= { 15 : "FizzBuzz", 7 : "Ruzz", 3 : "Fizz", 5 : "Buzz"}
 
-def fizz_numbers_division (number):
-    for num in div_return_fizz:
-        if number % num == 0:
-            return True
-def add_fizz_numbers(number):
-    div_return_fizz.append(number)
+# def fizz_buzz(number):
+#     if number % 5 == 0 and fizz_numbers_division():
+#         return "FizzBuzz"
+#     elif number % 3 == 0:
+#         return "Fizz"
+#     elif number % 5 == 0:
+#         return "Buzz"
+#     else:
+#         return str(number)
+
+
+
+def fizzbuzz (number):
+    for key in values_to_return:
+        if number % key == 0:
+            return values_to_return[key]
+    return str(number)
+        
--- tests/fizz_buzz_test.py
+++ tests/fizz_buzz_test.py
@@ -4,20 +4,23 @@ from src.fizz_buzz import *
 class TestFizzBuzz(unittest.TestCase):
 
     def setUp(self):
-        self.number1 = 23
+        self.number1 = 7
         self.number2 = 9
         self.number3 = 25
         self.number4 = 30
+        self.number5 = 59
 
-    def test_fizz_buzz_return_number(self):
-        self.assertEqual("23",fizz_buzz(self.number1))
+    def test_fizz_buzz_return_ruzz(self):
+        self.assertEqual("Ruzz",fizzbuzz(self.number1))
         
-
     def test_fizz_buzz_return_Fizz(self):
-        self.assertEqual("Fizz",fizz_buzz(self.number2))
+        self.assertEqual("Fizz",fizzbuzz(self.number2))
 
     def test_fizz_buzz_return_Buzz(self):
-         self.assertEqual("Buzz",fizz_buzz(self.number3))
+         self.assertEqual("Buzz",fizzbuzz(self.number3))
 
     def test_fizz_buzz_return_FizzBuzz(self):
-        self.assertEqual("FizzBuzz",fizz_buzz(self.number4))
+        self.assertEqual("FizzBuzz",fizzbuzz(self.number4))
+
+    def test_fizz_buzz_return_number(self):
+        self.assertEqual("59",fizzbuzz(self.number5))
